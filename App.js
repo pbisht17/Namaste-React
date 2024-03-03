00b9cd7b27306bf12730cc2170a09166fede8075
unknown <pawanbisht83@gmail.com>
2024-03-03 14:45:07 +0530

    First episode complete

---
+++ App.js
@@ -0,0 +1,22 @@
+const root = ReactDOM.createRoot(document.getElementById('root'));
+const heading = React.createElement('h1', {id: "heading"}, 'Hello Namaste React');
+const child = React.createElement(
+     'div', { id: 'parent'},  
+         [ 
+      React.createElement('div', {id: 'child'}, 
+         [  
+            React.createElement('h1', {id: "heading"}, 'Hello Namaste React'),
+            React.createElement('h2', {id: "heading"}, 'Hello Namaste React h2'), // Siblings are shown using Array of React.createElement()
+ ]
+ ),
+      React.createElement('div', {id: 'child'}, 
+    [  
+            React.createElement('h1', {id: "heading"}, 'Hello Namaste React'),
+            React.createElement('h2', {id: "heading"}, 'Hello Namaste React h2'), // Siblings are shown using Array of React.createElement()
+    ]) 
+]
+)
+const parent = React.createElement('div', {id: 'parent'}, child )
+
+
+root.render(child)
