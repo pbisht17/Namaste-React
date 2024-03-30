bca7d90a7bddd13748e730e71671ac4f0a840a08
pbisht17 <pawanbisht94.pb@gmail.com>
2024-03-30 17:10:26 +0530

    Adding Gitignore

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 const root = ReactDOM.createRoot(document.getElementById('root'));
 const heading = React.createElement('h1', {id: "heading"}, 'Hello Namaste React');
 const child = React.createElement(
-     'div', { id: 'parent'},  
+     'div', { id: 'parent'},
          [ 
       React.createElement('div', {id: 'child'}, 
          [  
